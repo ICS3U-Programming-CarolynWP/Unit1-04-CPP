4a71fcf1304af1a6571f1078cd78be3ac4e24573
Carolyn Webster Pless <carolyn.webster-pless@stu.ocsb.ca>
2022-09-17 01:36:58 +0000

    Finished coding

---
+++ areaPerRect.cpp
@@ -0,0 +1,12 @@
+// Copyright 2022: Carolyn Webster Pless
+// Created on: 22/09/16
+// Displays rectangle with length of 20m and width of 34m
+
+#include <iostream>
+
+int main() {
+    std::cout << "Rectangle Area and Perimeter:\n";
+    std::cout << "length is 20m and width is 34m:\n";
+    std::cout << "The area is = " << 20 * 34 << " m^2\n";
+    std::cout << "The perimeter is = " << 2 * (20 + 34) << " m\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright 2022: Carolyn Webster Pless
+// Created on: 22/09/16
+// Does math calculations
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "Some math:\n";
+    std::cout << "17 + 8 = " << 17 + 8 << std::endl;
+    std::cout << "23 - 10 = " << 23 - 10 << std::endl;
+    std::cout << "38 * 3 = " << 38 * 3 << std::endl;
+    std::cout << "95 / 6 = " << 95 / 6 << std::endl;
+    std::cout << "5^8 = " << pow(5, 8) << std::endl;
+    std::cout << "√37 = " << sqrt(37) << std::endl;
+}
